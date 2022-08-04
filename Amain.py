486400175e31076864de3011feeb919afbce34cb
ElnurSh <elnur.shiri@gmail.com>
2022-08-04 16:27:22 +0400

    04.08.2022

--- Amain.py
+++ Amain.py
@@ -1,3 +1,4 @@
+import aiogram.types
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
@@ -20,14 +21,21 @@ dp = Dispatcher(bot)
 order_btn = InlineKeyboardButton('🛍 Sifariş et!', callback_data='order')
 back_btn = InlineKeyboardButton('🔙 Geri', callback_data='back')
 next_btn = InlineKeyboardButton('Növbəti ➡️', callback_data='next')
-btn2 = InlineKeyboardMarkup().add(order_btn).row(next_btn) # order order and next row button
-btn3 = InlineKeyboardMarkup().add(order_btn).row(back_btn, next_btn) # order button and back, next row buttons
+start_btn = InlineKeyboardMarkup().add(order_btn).row(next_btn)
+middle_btn = InlineKeyboardMarkup().add(order_btn).row(back_btn, next_btn)
+end_btn = InlineKeyboardMarkup().add(order_btn).row(back_btn)
 
 
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await bot.send_message(message.from_user.id,"Hello! Write me something!",reply_markup=btn2)
+    await bot.send_photo(message.chat.id, photo=open('1.jpg', 'rb'), caption='Hello! Write me something! 1', reply_markup=start_btn)
+
+
+@dp.callback_query_handler(text='next')
+async def next_photo(call: types.CallbackQuery):
+    await bot.delete_message(call.from_user.id, call.message.message_id)
+    await bot.send_photo(call.message.chat.id, photo=open('2.jpg', 'rb'), caption='Hello! Write me something! 2', reply_markup=middle_btn)
 
 
 if __name__ == '__main__':
